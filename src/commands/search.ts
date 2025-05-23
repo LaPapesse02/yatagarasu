2c1d82ee03f564b6cd64e741f7fd9b577daec702
LaPapesse <lapapesse02@protonmail.com>
2025-05-23 16:02:08 +0200

    added options to command

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,9 +1,22 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { search } from "../mangaupdates";
 
 
 export const data = new SlashCommandBuilder()
     .setName('search')
     .setDescription('Searches a manga')
+
+    .addStringOption(option => 
+        option.setName('series')
+            .setDescription('The series to search')
+            .setRequired(true)
+    )
+
+    .addBooleanOption(option =>
+        option.setName('allow_explicit')
+            .setDescription('Allow hentai in the search results')
+            .setRequired(false)
+    )
 
     .addBooleanOption(option =>
         option.setName('ephemeral')
@@ -12,5 +25,5 @@ export const data = new SlashCommandBuilder()
     );
 
 export const execute = async (interaction: ChatInputCommandInteraction) => {
-    
+    await search(interaction);
 };
