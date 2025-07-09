c5867775b00fd61f5f2f806e1a13d997bd4929f2
LaPapesse <lapapesse02@protonmail.com>
2025-07-09 04:05:41 +0200

    added selection menu to search response

--- src/embeds.ts
+++ src/embeds.ts
@@ -1,8 +1,16 @@
-import { Colors, EmbedBuilder } from "discord.js";
+import { ActionRowBuilder, Colors, EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from "discord.js";
 import type { RequestResult } from "./@types/discord.t";
 
 
 const MAX_TITLE_LENGTH = 50;
+
+const shortenTitle = (title: string) => {
+        if (title.length > MAX_TITLE_LENGTH) {
+            return `${title.slice(0, MAX_TITLE_LENGTH - 1)}…`;
+        } else {
+            return title;
+        }
+}
 
 export const SEARCHING_EMBED = new EmbedBuilder()
     .setTitle('Searching manga...');
@@ -11,19 +19,32 @@ export const generateResultListEmbed = (resultList: RequestResult[]) => {
     let embedContent = '';
     let counter = 1;
     resultList.forEach(result => {
-        embedContent = embedContent.concat(`${counter}. `);
-
-        if (result.title.length > MAX_TITLE_LENGTH) {
-            embedContent = embedContent.concat(`**${result.title.slice(0, MAX_TITLE_LENGTH - 1)}…**`);
-        } else {
-            embedContent = embedContent.concat(`**${result.title}**`);
-        }
-
-        embedContent = embedContent.concat(` (${result.year})\n`);
+        embedContent = embedContent.concat(`${counter}. **${shortenTitle(result.title)}** (${result.year})\n`);
         counter++;
     })
 
     return new EmbedBuilder()
         .setDescription(embedContent)
         .setColor(Colors.Green)
+}
+
+export const generateResultListComponents = (resultList: RequestResult[]) => {
+    const optionsList: StringSelectMenuOptionBuilder[] = [];
+    let counter = 1;
+
+    resultList.forEach(result => {
+        optionsList.push(
+            new StringSelectMenuOptionBuilder()
+                .setLabel(shortenTitle(`${counter}. ${shortenTitle(result.title)}`))
+                .setValue(result.id.toString())
+        )
+        counter++
+    })
+
+    const select = new StringSelectMenuBuilder()
+        .setCustomId('result_selection')
+        .setPlaceholder('Choose a result!')
+        .addOptions(optionsList);
+
+    return new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(select);
 }
--- src/mangaupdates.ts
+++ src/mangaupdates.ts
@@ -1,19 +1,22 @@
-import { ChatInputCommandInteraction, MessageFlags } from "discord.js"
-import { generateResultListEmbed, SEARCHING_EMBED } from "./embeds"
+import { ChatInputCommandInteraction, EmbedBuilder, InteractionCallbackResponse, MessageFlags } from "discord.js"
+import { generateResultListComponents, generateResultListEmbed, SEARCHING_EMBED } from "./embeds"
 import type { RequestResult } from "./@types/discord.t"
 
 
 const MAX_RESULT_LIST_LENGTH = 10
 const SEARCH_SERIES_API = 'https://api.mangaupdates.com/v1/series/search'
+const INTERACTION_TIMEOUT = 60
 
 export const search = async (interaction: ChatInputCommandInteraction) => {
+    let response;
     if (interaction.options.getBoolean('ephemeral') ?? true) {
-        interaction.reply({
+        response = await interaction.reply({
             embeds: [ SEARCHING_EMBED ],
-            flags: MessageFlags.Ephemeral
+            flags: MessageFlags.Ephemeral,
+            withResponse: true
         })
     } else {
-        interaction.reply({ embeds: [ SEARCHING_EMBED ] })
+        response = await interaction.reply({ embeds: [ SEARCHING_EMBED ], withResponse: true })
     }
 
     const results: any = await getSearchResultList(
@@ -23,8 +26,10 @@ export const search = async (interaction: ChatInputCommandInteraction) => {
 
     const parsedResults = await parseResultList(results.results as object[]);
     const resultEmbed = generateResultListEmbed(parsedResults);
+    const selectionRow = generateResultListComponents(parsedResults);
 
-    interaction.editReply({ embeds: [ resultEmbed ] });
+    const a = await interaction.editReply({ embeds: [ resultEmbed ], components: [ selectionRow ] });
+    handleInteraction(response, interaction, resultEmbed)
 }
 
 const getSearchResultList = async (query: string, allowExplicit: boolean): Promise<object> => {
@@ -59,3 +64,18 @@ const parseResultList = async (results: any[]) => {
 
     return parsedResults;
 }
+
+const handleInteraction = async (response: InteractionCallbackResponse, interaction: ChatInputCommandInteraction, originalEmbed: EmbedBuilder) => {
+    const collectorFilter = (i: any) => i.user.id === interaction.user.id;
+
+    try {
+        const confirmation = await response.resource?.message?.awaitMessageComponent({ filter: collectorFilter, time: INTERACTION_TIMEOUT * 1000 });
+
+        if (confirmation!.customId === 'result_selection') {
+            // TODO: respond to user selection
+        }
+    } catch {
+        interaction.editReply({embeds: [ originalEmbed.setFooter({ text: `No interaction received within ${INTERACTION_TIMEOUT}s` }) ], components: [] })
+    }
+}
+
