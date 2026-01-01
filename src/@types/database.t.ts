b27a590f9eb2f63459a024bfcf547a4d4d2bc41a
LaPapesse02 <lapapesse02@protonmail.com>
2026-01-01 17:21:02 +0100

    added types for the tables in the database

--- src/@types/database.t.ts
+++ src/@types/database.t.ts
@@ -2,3 +2,33 @@ export interface SubscribedSeries {
     user_id: string,
     series_id: string
 }
+
+export interface Author {
+    series_id: string,
+    author_id: string,
+    name: string,
+    role: string
+}
+
+export interface Genre {
+    series_id: string,
+    genre: string
+}
+
+export interface Series {
+    id: string,
+
+    title: string,
+    description: string | null,
+    year: string,
+    completed: boolean,
+    genres: Genre[] | undefined,
+    authors: Author[] | undefined,
+
+    url: string,
+    image_url: string,
+
+    latest_chapter: number,
+
+    last_modified: Date
+}
