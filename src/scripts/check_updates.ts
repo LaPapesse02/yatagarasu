f34358f6b6878159fbd7fb3d7439422e584ef5b6
LaPapesse02 <lapapesse02@protonmail.com>
2026-02-22 04:54:34 +0100

    added function to get the users that should be notified

--- src/scripts/check_updates.ts
+++ src/scripts/check_updates.ts
@@ -1,9 +1,9 @@
 import { Series } from "../@types/database.t";
-import { getCachedLatestChapter, getSubscribedSeries } from "./database"
+import { getCachedLatestChapter, getSubscribedSeries, getUsersSubscribed } from "./database"
 import { getSeries } from "./mangaupdates";
 
 /**
- * returns all series that
+ * returns all series that were updated
  */
 const getUpdates = async () => {
     const updatedSeries: Series[] = [];
@@ -17,4 +17,25 @@ const getUpdates = async () => {
             updatedSeries.push(seriesInfo)
     }
 
+}
+
+/**
+ * get all users that need to be notified about an update
+ */
+const createNotificationLists = async (updatedSeries: Series[]) => {
+    const notifications = new Map<string, Series[]>();
+    
+    for (let series of updatedSeries) {
+        const usersSubsribed = await getUsersSubscribed(series.id);
+
+        for (let user of usersSubsribed) {
+            if (notifications.has(user.user_id)) {
+                notifications.get(user.user_id)?.push(series);
+            } else {
+                notifications.set(user.user_id, [ series ])
+            }
+        }
+    }
+
+    return notifications;
 }
